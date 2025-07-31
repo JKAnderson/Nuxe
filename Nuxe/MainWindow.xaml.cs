59f128057fcb12b916c315f281dbc9f2ce3e636d
Joseph Anderson <josepha93@gmail.com>
2025-07-31 00:42:16 -0400

    Minimum viable layout

--- Nuxe/MainWindow.xaml.cs
+++ Nuxe/MainWindow.xaml.cs
@@ -1,24 +1,17 @@
-﻿using System.Text;
+﻿using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Nuxe
+namespace Nuxe;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public static string TitleText => $"Nuxe {Assembly.GetExecutingAssembly().GetName().Version.ToString(3)}";
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
